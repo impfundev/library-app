138a015b9c9f2681a75d0447728816b095d8499b
impfundev <mail@ilhammaulana.me>
2024-06-30 12:54:27 +0700

    fix: rename settings dir as config

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "library_django.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "library_django.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
