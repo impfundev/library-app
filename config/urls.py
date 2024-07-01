82477bc6916120a33ecb6e6bf22257c5ad21620f
impfundev <mail@ilhammaulana.me>
2024-07-01 12:18:22 +0700

    feat: add homepage

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from dashboards.views import home
 
 urlpatterns = [
+    path("", home, name="homepage"),
     path("dashboard/", include("dashboards.urls")),
     path("admin/", admin.site.urls),
     path("auth/", include("authentications.urls")),
