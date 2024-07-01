2276fb54528276cc49314f12195437fbfe868b42
impfundev <mail@ilhammaulana.me>
2024-07-01 17:50:52 +0700

    feat: reports include librarians login history

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from librarians.models import LoginHistory
+from members.models import Members
+from book_loans.models import Book, BookLoans
 
 
 def home(request):
@@ -6,4 +9,15 @@ def home(request):
 
 
 def index(request):
-    return render(request, "dashboard/index.html")
+    latest_login_history = LoginHistory.objects.order_by("login_at")[:10]
+    total_book = Book.objects.count()
+    total_member = Members.objects.count()
+    total_book_loans = BookLoans.objects.count()
+    context = {
+        "login_histories": latest_login_history,
+        "total_book": total_book,
+        "total_member": total_member,
+        "total_book_loans": total_book_loans,
+    }
+
+    return render(request, "dashboard/index.html", context)
