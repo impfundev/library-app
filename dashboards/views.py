ab59db25cae0a07eb2deb3b7e6d0f5ae3dd7225b
impfundev <mail@ilhammaulana.me>
2024-07-02 16:31:23 +0700

    fix: empty book loan

--- dashboards/views.py
+++ dashboards/views.py
@@ -18,22 +18,23 @@ def index(request):
     now = datetime.now()
     overdue_loans = BookLoans.objects.filter(due_date__lte=now)
 
-    for loan in BookLoans.objects.all():
-        near_overdue_window = loan.due_date - timedelta(days=7)
-        due_date = loan.due_date
-
-    near_overdue_loans = BookLoans.objects.filter(
-        due_date__range=(near_overdue_window, due_date)
-    )
-
     context = {
         "login_histories": latest_login_history,
         "total_book": total_book,
         "total_member": total_member,
         "total_book_loans": total_book_loans,
         "total_overdue": overdue_loans.count(),
-        "overdue_loans": overdue_loans,
-        "near_overdue_loans": near_overdue_loans,
     }
+
+    if overdue_loans.exists():
+        context["overdue_loans"] = overdue_loans
+        for loan in BookLoans.objects.all():
+            near_overdue_window = loan.due_date - timedelta(days=7)
+            due_date = loan.due_date
+
+        near_overdue_loans = BookLoans.objects.filter(
+            due_date__range=(near_overdue_window, due_date)
+        )
+        context["near_overdue_loans"] = near_overdue_loans
 
     return render(request, "dashboard/index.html", context)
