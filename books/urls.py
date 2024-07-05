da93d616a9d16a2eb5aea8c7bfdfb9f0836b6d91
impfundev <mail@ilhammaulana.me>
2024-07-05 14:38:45 +0700

    fix: book dashboard cache

--- books/urls.py
+++ books/urls.py
@@ -4,12 +4,11 @@ from django.views.decorators.cache import cache_page
 from django.conf import settings
 
 urlpatterns = [
-    # path(
-    #     "",
-    #     cache_page(settings.CACHE_TTL, key_prefix="books")(index),
-    #     name="book_list",
-    # ),
-    path("", index, name="book_list"),
+    path(
+        "",
+        cache_page(settings.CACHE_TTL, key_prefix="books")(index),
+        name="book_list",
+    ),
     path("<int:id>/update/", update, name="book_update"),
     path("<int:id>/delete/", delete, name="book_delete"),
 ]
