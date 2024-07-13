7fb5f0033be7cae9736a9f46a2ff412f502aa781
impfundev <ilhammaulana.dev@gmail.com>
2024-07-13 22:31:03 +0700

    fix: routers member include serializer class, not viewset

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import LibrarianViewSet, MemberSerializer
+from .views import LibrarianViewSet, MemberViewSet
 
 router = routers.DefaultRouter()
 router.register(r"librarians", LibrarianViewSet, basename="librarians")
-router.register(r"members", MemberSerializer, basename="members")
+router.register(r"members", MemberViewSet, basename="members")
 
 urlpatterns = [
     path("", include(router.urls)),
