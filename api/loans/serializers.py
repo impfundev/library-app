81f82bd88cda67b0b85b00239aa2800c6886f204
impfundev <ilhammaulana.dev@gmail.com>
2024-07-14 23:38:16 +0700

    feat: member loan api, feat: nested book loan api

--- api/loans/serializers.py
+++ api/loans/serializers.py
@@ -1,9 +1,28 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from loans.models import BookLoan
+from ..book.serializers import BookSerializer
+from ..auth.serializers import MemberSerializer
 
 
 class BookLoanSerializer(serializers.ModelSerializer):
+    book_detail = BookSerializer(source="book", read_only=True)
+    member_detail = MemberSerializer(source="member", read_only=True)
+
     class Meta:
         model = BookLoan
         fields = "__all__"
+
+
+class MemberLoanSerializer(BookLoanSerializer):
+    is_overdue = serializers.BooleanField(read_only=True)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["is_overdue"] = instance.due_date < timezone.now()
+        return data
+
+    class Meta:
+        model = BookLoan
+        fields = ["book", "loan_date", "due_date", "is_overdue"]
