80972dba11c1396a4d6da4a272bf23750b7e077b
impfundev <ilhammaulana.dev@gmail.com>
2024-07-16 12:54:25 +0700

    fix: add created_at and updated_at field to book loan model

--- loans/models.py
+++ loans/models.py
@@ -11,3 +11,5 @@ class BookLoan(models.Model):
     loan_date = models.DateTimeField()
     due_date = models.DateTimeField()
     return_date = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
