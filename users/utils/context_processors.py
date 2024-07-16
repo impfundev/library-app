7fff24bed5bc83b6d31bf5ab07131fa9a2599229
impfundev <ilhammaulana.dev@gmail.com>
2024-07-16 13:35:06 +0700

    fix: handle unauthenticated user context

--- users/utils/context_processors.py
+++ users/utils/context_processors.py
@@ -2,7 +2,9 @@ from ..models import Librarian
 
 
 def user_context(request):
-    user = request.user
-    librarian = Librarian.objects.get(user=user)
+    if request.user.is_authenticated:
+        user = request.user
+        librarian = Librarian.objects.get(user=user)
+        return {"user": user, "librarian_id": librarian.id}
 
-    return {"user": user, "librarian_id": librarian.id}
+    return {"user": None, "librarian_id": None}
