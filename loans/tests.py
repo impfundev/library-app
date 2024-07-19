91432e0307af4678fa608579f8bd5cdf36b59e7f
impfundev <ilhammaulana.dev@gmail.com>
2024-07-19 16:21:09 +0700

    fix: unsed package in test case

--- loans/tests.py
+++ loans/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 
 from .models import BookLoan, Book, Member
 
